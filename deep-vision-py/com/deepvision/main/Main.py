b785968e452d4bdc9ae5e48d52e027868262dfe1
Arpit Kumar <itsarpitgupta@gmail.com>
2019-04-17 22:06:56 +0530

    second commit

--- deep-vision-py/com/deepvision/main/Main.py
+++ deep-vision-py/com/deepvision/main/Main.py
@@ -1,12 +1,9 @@
 from com.deepvision.tools.TemplateMatching import TemplateMatching
 
-class Main(object):
-
-    def main(self):
-        print("In Main");
-        temp = TemplateMatching();
-        temp.__int__();
+def main():
+    print("In Main");
+    temp = TemplateMatching();
+    # temp.__int__();
 
 if __name__ == "__main__":
-    main = Main();
-    main.main();
+    main();
