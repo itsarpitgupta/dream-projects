50f3eaa38e7171073cc5fb3315c76b96ec2fbd3a
Arpit Kumar <itsarpitgupta@gmail.com>
2019-08-17 10:12:40 +0530

    setup image stacked widget added

---
+++ deepvision-ui/get_connection.py
@@ -0,0 +1,10 @@
+from PySide2.QtWidgets import QStackedWidget, QListWidget
+
+
+def get_connection_click_event(self,stackwidget:QStackedWidget):
+    self.stackedWidget.setCurrentIndex(0)
+    # listwidget.insertItem()
+
+def get_connected_devices_info():
+    print("pendadf")
+
--- deepvision-ui/test.py
+++ deepvision-ui/test.py
@@ -2,6 +2,8 @@ import sys
 
 from PyQt5 import QtCore
 from PySide2.QtWidgets import QApplication, QMainWindow, QAction
+
+from get_connection import get_connection_click_event
 from gui.ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -10,7 +12,11 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(0))
+        # self.ui.pushButton.clicked.connect(lambda : get_connection_click_event(self.ui.stackedWidget))
         self.ui.pushButton_2.clicked.connect(lambda : self.ui.stackedWidget.setCurrentIndex(1))
+        self.ui.pushButton_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(2))
+        self.ui.statusbar.addPermanentWidget(self.ui.progressBar)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
